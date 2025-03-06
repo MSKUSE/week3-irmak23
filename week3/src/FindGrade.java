a2844b9bea4f2c136796baf38f1472e34005a449
Irmak Köse <irmakkose@posta.mu.edu.tr>
2025-03-06 12:37:07 +0300

    Week3 is done

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -10,13 +10,30 @@ public class FindGrade{
         // 70 - 79 -> C
         // 60 - 69 -> D
         // 0 - 59 -> F
+        int length_of_args = args.length;
+        if(length_of_args == 0){
+            System.out.println("You should give a score");
+        }
+        else {
+            int grade = Integer.parseInt(args[0]);
 
 
-
-
+            if (grade >= 90) {
+                System.out.println("Grade: A");
+            } else if (grade >= 80 && grade < 89) {
+                System.out.println("Grade: B");
+            } else if (grade >= 70 && grade < 79) {
+                System.out.println("Grade: C");
+            } else if (grade >= 60 && grade < 69) {
+                System.out.println("Grade: D");
+            } else if (grade >= 0 && grade < 59) {
+                System.out.println("Grade: F");
+            } else {
+                System.out.println("invalid score");
+            }
+        }
 
     }
-
 
 
 }
